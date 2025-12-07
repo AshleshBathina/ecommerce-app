ee0c37286a48c0d4591d5bd46b91a0430e009e56
Ashlesh Bathina <ashleshbathina@gmail.com>
2025-12-07 13:19:08 +0000

    first deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +12,17 @@ app.get("/api/health", (req, res) => {
   res.send("OK");
 })
 
-app.listen(PORT, () => {
+//make our app ready for deployment
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
 });
 
