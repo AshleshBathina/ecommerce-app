86ae467409b5282ef7a64da8e0c8de0a3bb934e7
Ashlesh Bathina <ashleshbathina@gmail.com>
2025-12-09 00:07:50 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,11 +5,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css'
 import App from './App.jsx'
 
-const PUBLISHABLE_KEY = import.meta.env.CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("no PUBLISHABLE_KEY found!");
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
