dc29c8c39ead1349927285094209c7ac45629533
Ashlesh Bathina <ashleshbathina@gmail.com>
2025-12-08 19:14:31 +0000

    feat: Integrate Clerk authentication in the frontend and add Cloudinary dependency to the backend.

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,11 +5,15 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css'
 import App from './App.jsx'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("no PUBLISHABLE_KEY found!");
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={"pk_test_ZG9taW5hbnQtamF5YmlyZC02Ni5jbGVyay5hY2NvdW50cy5kZXYk"}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </StrictMode>,
