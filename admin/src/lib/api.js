d1f699f2c7975169d9bddd5ba3e19b6cfa6eccb7
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-01-01 18:22:22 +0000

    feat: Implement dashboard statistics and recent orders display using new API services, and add user profile to sidebar.

---
+++ admin/src/lib/api.js
@@ -0,0 +1,38 @@
+import { data } from "react-router"
+import axiosInstance from "./axios"
+
+export const productApi = {
+  getAll: async () => {
+    await axiosInstance.get("/admin/products")
+    return data;
+  },
+
+  create: async (formData) => {
+    const { data } = await axiosInstance.post("/admin/prodcuts", formData)
+    return data;
+  },
+
+  update: async ({ id, formData }) => {
+    const { data } = await axiosInstance.put(`/admin/products/${id}`, formData);
+    return data;
+  }
+}
+
+export const ordersApi = {
+  getAll: async () => {
+    const { data } = await axiosInstance.get("/admin/orders")
+    return data
+  },
+
+  updateStatus: async ({ orderId, status }) => {
+    const { data } = await axiosInstance.patch(`/admin/orders/${orderId}/status`, { status })
+    return data;
+  }
+}
+
+export const StatsApi = {
+  getDashboard: async () => {
+    const { data } = await axiosInstance.get('/admin/stats');
+    return data;
+  }
+}
--- admin/src/pages/DashboardPage.jsx
+++ admin/src/pages/DashboardPage.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query"
+import { ordersApi, StatsApi } from "../lib/api"
+
+import { IndianRupee, PackageIcon, ShoppingBagIcon } from "lucide-react"
 
 function DashboardPage() {
+  const { data: ordersData, isLoading: ordersLoading } = useQuery({
+    queryKey: ["orders"],
+    queryFn: ordersApi.getAll,
+  })
+
+  const { data: statsData, isLoading: statsLoading } = useQuery({
+    queryKey: ["dashboardStats"],
+    queryFn: StatsApi.getDashboard
+  })
+
+  const recentOrders = ordersData.slice(0, 5) || []
+
+  const statsCards = [
+    { name: "Total Revenue", value: statsLoading ? "..." : `₹${statsData?.data.totalRevenue.toFixed(2) || 0}`, icon: <IndianRupee className="size-8" /> },
+    { name: "Total Orders", value: statsLoading ? "..." : statsData?.data.totalOrders || 0, icon: <ShoppingBagIcon className="size-8" /> },
+    {
+      name: "Total Customers",
+      value: isLoading ? "..." : statsData?.totalCustomers || 0,
+      icon: <UsersIcon className="size-8" />
+    },
+    {
+      name: "Total Products",
+      value: isLoading ? "..." : statsData?.totalProducts || 0,
+      icon: <PackageIcon className="size-8" />
+    },
+  ]
+
   return (
     <div>DashboardPage</div>
   )
