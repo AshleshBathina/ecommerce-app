e9d726a541e10e1c35bc71f4b011ac88fe955bee
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-01-31 17:36:16 +0000

    feat: Implement product filtering by search query and category selection on the home page, and correct `useQuery` hook usage.

--- frontend/src/hooks/useProducts.jsx
+++ frontend/src/hooks/useProducts.jsx
@@ -1,16 +1,14 @@
-import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { productsApi } from "../lib/api";
 
 const useProducts = () => {
 
-  useEffect(() => {
-    const data = useQuery({
-      queryKey: ['products'],
-      queryFn: productsApi.getAllProducts
-    })
-  }, [])
-  console.log(data, "Ashop")
+
+  const data = useQuery({
+    queryKey: ['products'],
+    queryFn: productsApi.getAllProducts
+  })
+
   return data;
 }
 
